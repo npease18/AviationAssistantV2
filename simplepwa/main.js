9a9d6ec4a62c4402a81e7cd747afef2df284f3c0
Nicholas Pease <me@nicholaspease.com>
2023-12-20 05:49:30 +0000

    Test OpenADSB Open on IOS

---
+++ simplepwa/main.js
@@ -0,0 +1,9 @@
+function main() {
+    document.getElementById("main_tab").style.display = "block";
+    document.getElementById("settings").style.display = "none";
+}
+
+function settings() {
+    document.getElementById("main_tab").style.display = "none";
+    document.getElementById("settings").style.display = "block";
+}
