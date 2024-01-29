8aefa1bda141e32e6e8fb25e820482b09b5ad0b5
Nicholas Pease <me@nicholaspease.com>
2024-01-29 05:42:04 +0000

    Visibility Fix

--- simplepwa/main.js
+++ simplepwa/main.js
@@ -56,8 +56,8 @@ function main() {
     document.getElementById("main_tab").style.display = "block";
     document.getElementById("settings").style.display = "none";
     document.getElementById("sidebar").style.display = "none";
-    document.getElementById("header_atc").style.display = "block";
-    document.getElementById("ATC_Button").style.visibility = "visible";
+    document.getElementById("header_atc").style.visibility = "visible";
+    document.getElementById("ATC_Button").style.display = "inline-block";
     document.getElementById("header").style.left = "210px"
 }
 
@@ -65,17 +65,17 @@ function settings() {
     document.getElementById("main_tab").style.display = "none";
     document.getElementById("settings").style.display = "block";
     document.getElementById("sidebar").style.display = "none";
-    document.getElementById("header_atc").style.display = "none";
-    document.getElementById("ATC_Button").style.visibility = "visible";
+    document.getElementById("header_atc").style.visibility = "hidden";
+    document.getElementById("ATC_Button").style.display = "inline-block";
     document.getElementById("header").style.left = "2%"
 }
 
 function ATC() {
     document.getElementById("main_tab").style.display = "block";
     document.getElementById("sidebar").style.display = "flex";
-    document.getElementById("ATC_Button").style.visibility = "hidden";
+    document.getElementById("ATC_Button").style.display = "none";
     document.getElementById("settings").style.display = "none";
-    document.getElementById("header_atc").style.display = "block";
+    document.getElementById("header_atc").style.visibility = "visible";
     document.getElementById("header").style.left = "10px"
 }
 
