819490f537612755f6610f205d2229e097300b66
Nicholas Pease <me@nicholaspease.com>
2024-01-31 04:53:40 +0000

    Backend ver bump

--- backend/python/backend.py
+++ backend/python/backend.py
@@ -4,7 +4,7 @@ import re
 
 app = Flask(__name__)
 name = "Aviation Assistant v2"
-software_version = "v.2.1.1"
+software_version = "v.2.1.2"
 tar1090_version = "dafba99"
 dump1090_version = "a80ba8f"
 
